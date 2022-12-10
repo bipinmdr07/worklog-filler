29265bba4e76aafa80cabb2752ce13f83bf61e3a
Bipin Manandhar <kubipin07@gmail.com>
2022-12-10 11:02:19 +0545

    Add popup.tsx inside src directory.

---
+++ src/popup.tsx
@@ -0,0 +1,16 @@
+function IndexPopup() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <a href="https://vyaguta.lftechnology.com/attendance/self/all" target="_blank">
+        Vyaguta Attendance
+      </a>
+    </div>
+  )
+}
+
+export default IndexPopup
