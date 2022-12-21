25122d0e7fd47961ce1acdf8a04664907233fd95
Bipin Manandhar <kubipin07@gmail.com>
2022-12-21 10:57:38 +0545

    Change the text and add hint for adding file location.

--- src/options.tsx
+++ src/options.tsx
@@ -8,9 +8,14 @@ const OptionsMenu = () => {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Select your preferred file type</h1>
+      <h1>Configuration</h1>
       <div style={{ marginTop: 8, marginBottom: 8 }}>
-        <input type="text" className="w-100" onChange={(e) => setText(e.target.value)} style={{ width: '100%' }} placeholder={fileURL || "Your todo file path"} value={text} accept=".txt,.org" />
+        <label>Enter the location of your todo file</label>
+        <input type="text" className="w-100" onChange={(e) => setText(e.target.value)} style={{ width: '100%' }} placeholder={fileURL || "file:///*/todo.(txt|org)"} value={text} accept=".txt,.org" />
+        <p>
+          <b>Hint * : </b>
+          Press <b>Ctrl + O</b> in the browser, select your todo file and copy and paste the link from address bar to above input field.
+        </p>
       </div>
       <button onClick={() => setFileURL(text)}>Save</button>
     </div>
