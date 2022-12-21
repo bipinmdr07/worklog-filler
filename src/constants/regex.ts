3f4ce15bb572dcdf48bd2513cd6ad40d52457007
Bipin Manandhar <kubipin07@gmail.com>
2022-12-21 23:20:03 +0545

    Add regex for date format and tag

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,1 +1,3 @@
 export const ORG_TIME_STAMP = /^[A-Z][a-z]{2}\s\d{4}\s([A-Z][a-z]+)\s\d{2}/
+export const TXT_TIME_STAMP = /\d{4}\-[0-1]\d\-[0-3]\d/ // YYYY-MM-DD
+export const TODO_TAG = /:[a-zA-Z]+:/
