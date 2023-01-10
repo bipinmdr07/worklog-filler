1130ca22919ebad27116708e717f0ea465d5022d
Bimochan Shrestha <bmochan@gmail.com>
2023-01-10 14:09:17 +0545

    tests: Add checks for invalid input

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -11,6 +11,12 @@
  * roundMins(1, 15) => 15
  */
 export const roundMins = (timeInMins: number, interval: number = 5) => {
+  if (timeInMins === null || isNaN(timeInMins)) {
+    throw new Error("Not a valid input");
+  }
+  if (timeInMins < 0 || interval <= 0) {
+    throw new RangeError("Number not in range");
+  }
   const hours = Math.floor(timeInMins / 60)
   const remainingMins = timeInMins - hours * 60
 
